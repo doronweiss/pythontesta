24ffaba909e4c0b7da3772e69908cce1e1c85c6d
doron <doron@10fs.co.il>
2019-02-15 16:51:21 +0200

    added yaniv

--- lambdatest.py
+++ lambdatest.py
@@ -1,6 +1,6 @@
 myf = lambda a,b: a+b
 
 def testfun (x, a, b):
-    return x (a,b) * 2.5
+    return x (a,b) * 2
 
 print (testfun(myf, 3, 4))
---
+++ typeshints.py
@@ -0,0 +1,4 @@
+def myfunc (x:str) ->str:
+    return 'Hello- ' + x
+
+print (type(myfunc(3)))
