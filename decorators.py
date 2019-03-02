74eccdf049c4a7817264371c02aa81f7e735d06b
doron <doron@10fs.co.il>
2019-03-02 22:08:33 +0200

    added yaniv

---
+++ decorators.py
@@ -0,0 +1,26 @@
+class testclass (object):
+    def __init__(self, anum):
+        self.number = anum
+
+
+    def show(self):
+        print ("Number = {}".format(self.number))
+
+    def showagain(self):
+        print ("Number = {}".format(self.number))
+
+
+def mydec(func):
+    def wrapper_do_twice():
+        func()
+        func()
+    return wrapper_do_twice
+
+@mydec
+def prrr(s):
+    print (s)
+
+tc = testclass(3)
+tc.show()
+tc.showagain()
+prrr("sdskjdksdj")
