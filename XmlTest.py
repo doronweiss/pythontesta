b5a53d98624ea088602bbe7b6c777899ae35e4f3
doron weiss <doron@10fs.co.il>
2019-09-02 18:12:24 +0300

    added XML test

---
+++ XmlTest.py
@@ -0,0 +1,18 @@
+import sys
+import xml.etree.ElementTree as ET
+
+fpath='c:/tries/Matlab/test1.xml '
+try:
+    tree = ET.parse(fpath)
+except Exception as e:
+    print("Could not load file {0}, {1}".format(fpath, e ))
+    sys.exit(-1)
+
+fileRoot = tree.getroot()
+dataRoot = fileRoot[0]
+cmds = dataRoot.findall ('command')
+if cmds is not None:
+    for cmd in cmds:
+        args = cmd.find ('./argument')
+        if args is not None:
+            print ("Args =  {0}".format(args.text.strip()))
