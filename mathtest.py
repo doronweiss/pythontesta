98eca276d344e53098b73f8a0d7aad1c507a7e26
doron <doron@10fs.co.il>
2020-04-11 22:07:03 +0300

    numpy changed matrix to array

--- mathtest.py
+++ mathtest.py
@@ -3,45 +3,46 @@ import numpy as np
 import scipy.integrate as spint
 import matplotlib.pyplot as plt
 
-a1 = np.array([1,2,3])
-a2=np.array([2,3,4])
-print ("Element wise => {}".format(a1*a2))
-print ("LinAlg wise => {}".format(a1@a2))
+a1 = np.array([1, 2, 3])
+a2 = np.array([2, 3, 4])
+print("Element wise => {}".format(a1 * a2))
+print("LinAlg wise => {}".format(a1 @ a2))
+av = np.vstack((a1, a2))
+print("av => {}".format(av))
+ah = np.hstack((a1, a2))
+print("ah => {}".format(ah))
 sys.exit(0)
 
+print("Version: %s" % (np.__version__))
 
-print ("Version: %s"%(np.__version__))
+a = 30
 
-a=30
+d2r = np.pi / 180.0
+ar = a * d2r
 
-d2r=np.pi/180.0
-ar=a*d2r
+tmat = np.array([[np.cos(ar), np.sin(ar), 1], [-np.sin(ar), np.cos(ar), 1], [np.sin(ar), -np.cos(ar), 1]])
+# a = np.matrix([[1,2,3],[3,4,5],[1,0,13]])
+print(tmat)
+print("Mat mult :")
+print(tmat * tmat.transpose())
+print("Determinant :", end="")
+print(np.linalg.det(tmat))
 
-tmat=np.array([[np.cos(ar), np.sin(ar),1], [-np.sin(ar), np.cos(ar), 1], [np.sin(ar), -np.cos(ar), 1]])
-#a = np.matrix([[1,2,3],[3,4,5],[1,0,13]])
-print (tmat)
-print ("Mat mult :")
-print (tmat*tmat.transpose())
-print ("Determinant :", end="")
-print (np.linalg.det(tmat))
-
-#speed test
-cnt=100
+# speed test
+cnt = 100
 start = time.time()
-for i in range(1,cnt):
-    c = tmat*tmat;
-print ("Matrix mult {} times, Elapsed: {} => {}".format (cnt, time.time() - start, c))
-
-
+for i in range(1, cnt):
+    c = tmat * tmat;
+print("Matrix mult {} times, Elapsed: {} => {}".format(cnt, time.time() - start, c))
 
 start = time.time()
 
-vec = np.array ([1.0, 0.0, 0.0])
-vec2 = np.array ([0.0, 1.0, 0.0])
-print (tmat@vec)
-print (np.cross(vec,vec2))
+vec = np.array([1.0, 0.0, 0.0])
+vec2 = np.array([0.0, 1.0, 0.0])
+print(tmat @ vec)
+print(np.cross(vec, vec2))
 
-print ("Elapsed: %s"%(time.time() - start))
+print("Elapsed: %s" % (time.time() - start))
 
 # plottinh
 r = range(101)
@@ -52,17 +53,18 @@ for i in r:
     y.append(np.sin(i / 10.0))
 plt.plot(x, y)
 plt.ylabel('Sinus')
-#plt.show()
+# plt.show()
 
-f= lambda x:x*x
+f = lambda x: x * x
 
-def myf (x):
+
+def myf(x):
     return np.sin(x)
 
-print ("Lambda 30 = {0}".format(f(30)))
+
+print("Lambda 30 = {0}".format(f(30)))
 
 i = spint.quad(myf, 0, np.pi)
-print ("Integration: i= {}".format(i))
+print("Integration: i= {}".format(i))
 i = spint.quad(lambda z: np.sin(z), 0, np.pi)
-print ("lambga Integration: i= {}".format(i))
-
+print("lambga Integration: i= {}".format(i))
