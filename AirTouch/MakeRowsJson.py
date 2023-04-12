c53c397059746cdf1222fbbfb8a0fca4e7bbb9f4
doron <doron@10fs.co.il>
2023-04-12 20:01:18 +0300

    minor formatting

--- AirTouch/MakeRowsJson.py
+++ AirTouch/MakeRowsJson.py
@@ -6,26 +6,26 @@ from json import JSONEncoder
 
 class Row:
     def __init__(self):
-        self.rowNum=0
-        self.direction=0
-        self.rowLengthCM=4000
+        self.rowNum = 0
+        self.direction = 0
+        self.rowLengthCM = 4000
         self.cleanSpeedM2M = 15
-        self.omitted=0
+        self.omitted = 0
 
 
 class RowsDef:
-    def __init__(self) -> object:
+    def __init__(self) -> None:
         self.rows = []
 
 
 rd = RowsDef()
-for idx in range(1,90):
+for idx in range(1, 90):
     r = Row()
-    r.rowNum=idx
-    r.direction =0 if idx % 2 == 0 else 1
+    r.rowNum = idx
+    r.direction = 0 if idx % 2 == 0 else 1
     rd.rows.append(r)
 
 print("has: {0} rows".format(len(rd.rows)))
 
 jsonString = json.JSONEncoder().encode(rd)
-print (jsonString)
+print(jsonString)
